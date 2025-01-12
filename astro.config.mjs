ca470f31e102d77a47376d3f4a53141c94293fbc
Pysio <qq593277393@outlook.com>
2025-01-12 18:15:46 +0800

    feat: 从obsidian同步

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,24 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import starlightObsidian, { obsidianSidebarGroup } from 'starlight-obsidian' // obsidian 插件
+
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'Pysio Docs',
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/pysio2007/docs',
 			},
+			plugins: [
+				starlightObsidian({
+					vault: './vault',
+				}),
+			],
 			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
+				obsidianSidebarGroup,
 			],
 		}),
 	],
